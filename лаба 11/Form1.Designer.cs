6c0d1e1c4be263f8db25559528d3c33ad2d475e7
79521 <79521@MSI>
2024-04-13 19:43:03 +0700

    Добавьте файлы проекта.

---
+++ лаба 11/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace лаба_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 383);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 45);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(198, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "РЕЗУЛЬТАТ";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(12, 182);
+            this.panel3.MaximumSize = new System.Drawing.Size(102, 130);
+            this.panel3.MinimumSize = new System.Drawing.Size(102, 38);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(102, 38);
+            this.panel3.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(-13, -1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "РАСЧЕТ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Цикл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Формула";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(445, 182);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(57, 38);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "C";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button4_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Location = new System.Drawing.Point(166, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 77);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(16, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите n";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(0, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(525, 120);
+            this.panel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 481);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ лаба 11/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using лаба_11.Properties;
+
+namespace лаба_11
+{
+    public partial class Form1 : Form
+    {
+        int n;
+        bool iscollapsed = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double formula(int n)
+        {
+            return Math.Pow(-1, n) * (n + 1);
+        }
+        private double whhile(int n)
+        {
+            double rez = 0;
+            while (n >= 0)
+            {
+                rez += Math.Pow(-1, n) * (2 * n + 1);
+                n--;
+            }
+            return rez;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) 
+        { 
+            if (iscollapsed) // пока не выполнится некое условие, таймер будет работать
+            {
+                panel3.Height += 5; // высота панели, в которой находится две три кнопки, причем две функцональные, за каждую итерацию увеличивается на 5 пикселей.
+                // для этого необходимо задать в свойствах панели минимальное и максимальное значение
+                if (panel3.Size == panel3.MaximumSize) // если панель достигает своих максимальных размеров, то таймер останавливается и метка присваивает значение false, чтобы при повторном нажатии выполнилось другое условие и панель закрылась
+                {
+                    timer1.Stop();
+                    iscollapsed = false;
+                }
+            }
+            else 
+            {
+                panel3.Height -= 5;
+                if (panel3.Size == panel3.MinimumSize)
+                {
+                    timer1.Stop();
+                    iscollapsed = true;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); // Запускает таймер по нажатию кнопки, ну то есть метод таймер, в котором вся необходимая логика уже расписана.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n = Convert.ToInt32(textBox2.Text);
+                if (n > int.MaxValue || n < int.MinValue)
+                {
+                    throw new OverflowException("Число должно входить в диапозон значений Int 32");
+                }
+                textBox1.Text = Convert.ToString(formula(n));
+            }
+            catch (OverflowException)
+            {
+                textBox1.Text = "Число должно входить в диапозон значений Int 32, Нажмите С";
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n = Convert.ToInt32(textBox2.Text);
+                if (n > int.MaxValue || n < int.MinValue)
+                {
+                    throw new OverflowException("Число должно входить в диапозон значений Int 32");
+                }
+                textBox1.Text = Convert.ToString(whhile(n));
+            }
+            catch (OverflowException)
+            {
+                textBox1.Text = "Число должно входить в диапозон значений Int 32, Нажмите С";
+                textBox2 = null;
+                n = 0;
+            }
+        }
+
+        private void button4_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox2.Text = null;
+            n = 0;
+        }
+
+
+
+
+
+        //private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        //{
+        //    if (scroll == hScrollBar1.Value)
+        //    {
+        //        textBox2.Location = new Point(textBox2.Location.X + 2); // изменяем позицию внутри панели с вводом
+        //        label1.Location = new Point(label1.Location.X + 2);
+        //        panel1.Location = new Point(panel1.Location.X + 2);
+        //    }
+        //    else if (scroll < hScrollBar1.Value)
+        //    {
+        //        textBox2.Location = new Point(textBox2.Location.X - 2);
+        //        label1.Location = new Point(label1.Location.X - 2);
+        //        panel1.Location = new Point(panel1.Location.X - 2);
+        //    }
+        //    else if (scroll > hScrollBar1.Value)
+        //    {
+        //        textBox2.Location = new Point(textBox2.Location.X + 2);
+        //        label1.Location = new Point(label1.Location.X + 2);
+        //        panel1.Location = new Point(panel1.Location.X + 2);
+        //    }
+        //    scroll = hScrollBar1.Value;
+        //}
+    }
+}
+
+//**************************
+//*** Randomize the list ***
+//**************************
